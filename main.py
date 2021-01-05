91b838ee166d1b622b74cf73be2b6978dc69682c
Bessonica <43250127+Bessonica@users.noreply.github.com>
2021-01-05 18:16:30 +0200

    Update main.py
    сделал прототип game over экрана(нужно еще ввести меню и перезапуск игры после смерти игрока)
    класс laser, что еще не готов.сегодня сделаю.
    два новых классов кораблей(red, blue)один будет идти на корабль и взрываться рядом с гг,а другой стреляет издалека и держит дистанцию с игроком
    за сегодня все должно быть сделано
    а завтра буду смотреть как все оптимизировать+ показать разные знания по проге

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ width = 1250
 height = 750
 
 size = (width, height)
-screen = pygame.display.set_mode(size) #WIN
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption("SpaceGame")
 
 
 #загружаем файлы из assets
+
+#!!! В КОНЦЕ КАЖДОГО image.load должен быть .convert() !!
 
 spaceShip_red = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 spaceShip_green = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -27,7 +29,7 @@ laser_yellow = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 laser_green = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 laser_blue = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 
-#BG
+
 background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (size))
 
 
@@ -39,7 +41,7 @@ class Ship:
         self.hp = hp
         self.img_ship = None
         self.img_laser = None
-        self.lasers = []
+        self.lasers = []   #тут будут хранится разные лазеры для разных классов кораблей
         self.coolD_counter = 0
 
 
@@ -54,6 +56,10 @@ class Ship:
         return self.img_ship.get_height()
 
 
+    def give_position(self):
+        return self.x, self.y
+
+
 #класс player наследует от ship
 class Player(Ship):
     def __init__(self, x, y, hp=100):
@@ -65,11 +71,14 @@ class Player(Ship):
 
 
 
+
+
 # как их запрограммировать ччто бы они следовали за тобой,стреляли по игроку?
 # типы врагов
-# мины/пилоты камикадзе что не могут стрелять но следуют за игроком
+# мины/пилоты камикадзе что не могут стрелять но следуют за игроком и взрываются рядом с ним
+
 #снайперы,что редко стреляют,но выстреливают один сплошной луч+держутся на расстоянии от гг
-#
+#или пусть снафперы делают один выстрел и отлетают в рандомную сторону
 #
 #ПЕРЕДЕЛАТЬ из enemy идут классы red enemy,blue и тд
 #у каждого корабля свои свойства(крансые=мины и тд)
@@ -84,14 +93,56 @@ class Enemy(Ship):
         super().__init__(x, y, hp)
         self.img_ship, self.img_laser = self.color_map[color]
 
-    def move(self, vel):
-        self.y += vel
+    # def move(self, vel):  #для каждого типа корабля свое передвижение,так что это немного бесполезно
+    #     self.y += vel
+
+
+#камикадзе- идут напрямую на игрока,медленные,при сближении с гг или смерти взрываются!!наносят урон как игроку так и врагам!!
+class RedEnemy(Enemy):
+
+
+    def __init__(self, x, y, color, hp = 100):
+        super().__init__(x, y, color, hp)
+
+#может переименовать move в attack?или отдельный метод attack, что внутри себя использует move?+++после части #управление
+#вместо move вписать attack
+    def move(self, vel = 1):  #корабль следует медленно за игроком
+        self.x -= vel
+
+    def attack(self):
+        self.move()
+
+#проблема как достать координату игроку в класс Redenemy
+
+
+#снайпер
+class BlueEnemy(Enemy):
+    def __init__(self, x, y, color, hp = 100):
+        super().__init__(x, y, color, hp)
+
+
+    def move(self, vel = 1):  #корабль отлетает от игрока в сторону и снова делает выстрел
+        self.y -= vel
+
+    def attack(self):
+        self.move()
+
+
+#подумай, лазер будет иметь в себе расположение игрока,или это будет определяться в методе attack(скорее всего в атак,ведь
+#у каждого корабля свой стиль атаки может быть
+class Laser:
+    def __init__(self, x, y, img):
+        pass
+
+
+
 
 
 def main():
     game = True
     lives = 4
     font = pygame.font.SysFont("comicsans", 50)
+    font_game_over = pygame.font.SysFont("comicsans", 70)  #?поменять цвет?
     fps = 60
     clock = pygame.time.Clock()
 
@@ -102,7 +153,8 @@ def main():
 
     player = Player(300, 650)
     player_velocity = 5
-
+    game_over = False    #перемення что бы знать когда создавать меню при проигрыше,перезагружать игру
+#________________________________________
 
     def window_redraw():
         screen.blit(background, (0, 0)) #на surface screen перемещаем изобр background на координаты 0, 0
@@ -114,28 +166,49 @@ def main():
 
         player.draw(screen)
 
+        if game_over:    #ввсти выход в меню и удалить всех врагов+вернуть hp игроку
+            label_game_over = font.render("GAME OVER BUDDY", 1, (255, 255, 255))
+            screen.blit(label_game_over, (width/2-label_game_over.get_width()/2, height/2))
 
 
         pygame.display.update()
-
+#_____________________________________
 
     while game:
         clock.tick(fps)
 
+        if lives <= 0 or player.hp <= 0:  #!!game over screen!!  game_over = true. ввсти выход в меню и удалить всех врагов+вернуть hp игроку
+            game_over = True
+
+
         # также здесь можно организовать переходна новый уровень
-        #спавн врагов
-        if len(enemies) == 0:
+        #спавн врагов  !!!на будущие- сделай спавн врагов рандомным,что бы на каждой новой волне было разное кол врагов
+        # random.choice(["red","blue", "green"])
+
+
+        if len(enemies) == 0:  #также если игрок все еще не начал новую игру(game_over=true)не спавнить врагов
             for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, width-100), random.randrange(10, 150), random.choice(["red", "blue", "green"]))
+                enemy = RedEnemy(random.randrange(50, width-100), random.randrange(10, 150), "red") #!!!!!!!!!!!!!!!redenemy = enemy
                 enemies.append(enemy)
                 # print(enemy.color_map)
-            #game = False
+
+            for i in range(wave_length):
+                enemy = BlueEnemy(random.randrange(50, width - 100), random.randrange(10, 150),
+                              random.choice(["blue"]))  #!!!
+                enemies.append(enemy)
+                # print(enemy.color_map)
+
+        #game = False
 
 
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 game = False
+
+
+
+
 
 #управление
         keys = pygame.key.get_pressed()
@@ -148,18 +221,28 @@ def main():
         if keys[pygame.K_w] and player.y - player_velocity >0:
             player.y -= player_velocity
 
+
+# здесь нужно ввест координату игрока в метод attack(сделал метод give_position у ship- можно применить у всех кораблей)
+        for enemy in enemies:
+            enemy.attack()  # 2 = enemy velocity
+
+
+
+        #print(player.give_position())
+        #print(enemies[0].give_position())
         window_redraw()
-
-
-
-
 
 
 
 main()
 
-
-
+# как получить расположение мыши
+# pg.mouse.get_pos()
+#
+#сделать коллизию - взрыв камикадзе
+#сделать стрельу для корабля игрока и убийство врагов
+#
+#
 
 
 
