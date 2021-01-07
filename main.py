fda31484e16ebd0f6e6dbbda4210e7ee2ee7a3bd
Bessonica <43250127+Bessonica@users.noreply.github.com>
2021-01-08 01:14:49 +0200

    наконец-то закончил
    убрал лишние комментарии
    теперь после смерти нельзя играть,можно начать игру заново
    написал в стартовом меню управление
    найс

--- main.py
+++ main.py
@@ -12,24 +12,24 @@ height = 750
 
 size = (width, height)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("SpaceGame")
+pygame.display.set_caption("SpaceGame, Panfilov")
 
 
 #загружаем файлы из assets
 
 
 
-spaceShip_red = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-spaceShip_blue = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+spaceShip_EnRed = pygame.image.load(os.path.join("assets", "Ship_EnemyRed.png"))
+spaceShip_EnBlue = pygame.image.load(os.path.join("assets", "Ship_EnemyBlue.png"))
 
-spaceShip_yellow = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+spaceShip_player = pygame.image.load(os.path.join("assets", "Ship_Player.png"))
 
-laser_red = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-laser_yellow = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
-laser_blue = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+laser_red = pygame.image.load(os.path.join("assets", "laser_red.png"))
+laser_yellow = pygame.image.load(os.path.join("assets", "laser_yellow.png"))
+laser_blue = pygame.image.load(os.path.join("assets", "laser_blue.png"))
 
 
-background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (size))
+background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background.jpg")), (size))
 
 
 #общий класс для кораблей,от него наследуют корабли врагов и корабль игрока
@@ -45,7 +45,6 @@ class Ship:
         self.img_laser = None
         self.lasers = []
         self.coolD_counter = 0
-
 
     def draw(self, window):
         screen.blit(self.img_ship, (self.x, self.y))
@@ -95,7 +94,7 @@ class Ship:
 class Player(Ship):
     def __init__(self, x, y,vel = 8 , hp=10):
         super().__init__(x, y, hp) #используем метод инит из ship
-        self.img_ship = spaceShip_yellow
+        self.img_ship = spaceShip_player
         self.img_laser = laser_yellow
         self.mask = pygame.mask.from_surface(self.img_ship)#коллизия!!!!
         self.hp_max = hp
@@ -127,59 +126,17 @@ class Player(Ship):
         self.hp_bar(screen)
 
 
-        # #angle = 90
-        # mouse =[]
-        # mouse.append(angle)
-        # dx = mouse[0][0] - self.x
-        # dy = mouse[0][1] - self.y
-        # rads = math.atan2(-dy, dx)
-        # rads %=2*math.pi
-        # degs = 6+math.degrees(rads)   #!!!
-        # print(self.rect.centery, "TOYOOOOOOOOO")
-        # player_img = pygame.transform.rotate(self.img_ship, degs)
-        # player_rec = player_img.get_rect(topleft=(self.x, self.y))
-        # screen.blit(player_img, player_rec.topleft)
-        # pygame.display.update()
-
-
-        #WORKSSSSSS
-        # player_img = pygame.transform.rotate(self.img_ship, angle)
-        # player_rec = player_img.get_rect(topleft=(self.x, self.y))  #или без аргумента topleft, тести
-        # print(player_rec, "player rect")
-        # screen.blit(player_img, player_rec.topleft)
-        # pygame.display.update()
-        #
-
-
-        # player_img = pygame.transform.rotate(self.img_ship, 20)
-        # player_rect = player_img.get_rect()
-        # player_rect.center = (self.x, self.y)
-        # screen.blit(player_img,player_rect )
-
-        # offset = (mouse[0] - self.x, mouse[1]-self.y )
-        # pygame.transform.rotate(self.img_ship, 50)
-        # print(offset)
+    def stop(self):
+        self.x = width + 50
+        self.y = height + 50
 
 
 
-
-
-
-
-# как их запрограммировать ччто бы они следовали за тобой,стреляли по игроку?
-# типы врагов
-# мины/пилоты камикадзе что не могут стрелять но следуют за игроком и взрываются рядом с ним
-
-#снайперы,что редко стреляют,но выстреливают один сплошной луч+держутся на расстоянии от гг
-#или пусть снафперы делают один выстрел и отлетают в рандомную сторону
-# можно ввести противника турель у кот не будет метода(из ship) cooldown-a
-#
 #у каждого корабля свои свойства(крансые=мины и тд)
 class Enemy(Ship):
     color_map = {
-        "red": (spaceShip_red, laser_red),
-        #"green":(spaceShip_green, laser_green),
-        "blue":(spaceShip_blue, laser_blue)
+        "red": (spaceShip_EnRed, laser_red),
+        "blue":(spaceShip_EnBlue, laser_blue)
     }
 
     def __init__(self, x, y, color, hp=100):
@@ -187,11 +144,9 @@ class Enemy(Ship):
         self.img_ship, self.img_laser = self.color_map[color]
         self.mask = pygame.mask.from_surface(self.img_ship)
 
-    # def move(self, vel):  #для каждого типа корабля свое передвижение,так что это немного бесполезно
-    #     self.y += vel
 
 
-#камикадзе- идут напрямую на игрока,медленные,при сближении с гг или смерти взрываются!!наносят урон как игроку так и врагам!!
+#камикадзе- идут напрямую на игрока,медленные,при сближении с гг взрываются наносят урон  игроку
 class RedEnemy(Enemy):
     colorShip = "red"
 
@@ -203,20 +158,13 @@ class RedEnemy(Enemy):
         self.vec_start = pygame.math.Vector2((player.x, player.y))
         self.vec_end = pygame.math.Vector2((x, y)).normalize()
 
-    #может переименовать move в attack?или отдельный метод attack, что внутри себя использует move?+++после части #управление
-    #вместо move вписать attack
-    # def move(self,x, y, vel = 5):  #корабль следует медленно за игроком
-    #     self.x += vel
-    #  движение выглядит неестественно.корабль !!!!не движется по вектору!!! исправит ь это
-    def move(self, player, vel = 1):  #корабль следует медленно за игроком.
-        #print("PLAYER POS", player.x, player.y)
+
+    def move(self, player, vel = 3):  #корабль следует медленно за игроком.
         v = Vector(player, self)
-        # print("MEGA VECTOR BLYAD", v.pointX, v.pointY)
+        # print("VECTOR POINTS",v.pointX, v.pointY)
         # print(v.Length())
 
 
-        #плохая версия,корабли не движутся по вектору.попробуй сделать так что бы на один кадр вычисляли небольшой вектор и
-        #на его координату(мал вектора) отправляли корабль
         if v != 0:
             if v.pointX > 0:
                 self.x += vel
@@ -236,66 +184,8 @@ class RedEnemy(Enemy):
 
 
 
-
-
-            #Y
-            # if v.pointY > 0:
-            #     self.y += vel
-            #     # self.y += vel
-            # elif v.pointY <= 0:
-            #     self.y -= vel
-            # else:
-            #     pass
-
-            #X
-            # if v.pointX > 0:
-            #     self.x += vel
-            #     #self.y += vel
-            # elif v.pointX <=0:
-            #     self.x -= vel
-            # else:
-            #     pass
-
-
-        #print("red vector", Vector(player, self))
-
-
-
-
-
-    def moveLeft(self, vel = 5):
-        self.x -=vel
-
-
-
-
-
-
-
-            # if self.x>= width:
-            #     self.x -= vel
-            # elif self.x <= 0:
-            #     self.x += vel
-
-
-
-        # dirvect = pygame.math.Vector2(x - self.x, y - self.y) # КОД ЧТО БЫ КРАСНЫЙ ПРЕСЛЕДОВАЛ ИГРОКА
-        # dirvect.normalize()
-        # dirvect.scale_to_length(vel)
-        # print("DIRVECT", dirvect)   #dirvect[0] = x
-
-
     def attack(self, player, laserVel, obj):  #x, y =player pos.  self.x, self.y = ENEMY POS
         self.move(player)
-        # self.laser_draw(laserVel, player)
-        # if random.randrange(0, 120) == 1:  #frames PER SECOND = 60, шанс попадания в 2 сек = 50%
-        #     self.shoot()
-
-       # print(x, y)
-       # self.move()
-
-#проблема как достать координату игроку в класс Redenemy
-
 
 #снайпер
 class BlueEnemy(Enemy):
@@ -304,24 +194,8 @@ class BlueEnemy(Enemy):
         super().__init__(x, y, color, hp)
 
 
-    # def move(self,x, y, vel = 5):  #корабль следует медленно за игроком
-    #     self.x += vel
-
     def move(self,player, vel = 5):  #корабль следует медленно за игроком
         pass
-        # vector = Vector(player, self)
-        # vel_x = vector[0]/50
-        # vel_y = vector[1]/50
-        # print("VEL X Y", vel_x, vel_y)
-        # self.x += vel_x
-        # self.y += vel_y
-        # print("PLAYER", player.x, player.y)
-        # print("ENEMY", self.x, self.y)
-        # print("vector lenthg", Vector(player, self))
-        #print("VECTOR", vectorX, vectorY)
-
-    def moveLeft(self, vel = 5):
-        self.x -=vel
 
     def attack(self, player, laserVel, obj):   #здесь применяем move и laser
         self.laser_draw(laserVel, obj)
@@ -330,23 +204,6 @@ class BlueEnemy(Enemy):
             self.shoot("up")
 
             #print(x, y)
-
-        #тест для что бы не писать лишний код в ивент лупе,нужно вставить в аргументы enemies, player, lives.как то геморно
-        # for enemy in enemies:
-        #     if Collide(enemy, player):
-        #         lives -= 1
-        #         enemies.remove(enemy)
-
-    # def shoot(self):
-    #     if self.coolD_counter == 0:
-    #         laser = Laser(self.x, self.y, self.img_laser)
-    #         self.lasers.append(laser)
-    #         self.coolD_counter = 1
-
-
-
-
-
 
 #____________________________________
 
@@ -359,7 +216,7 @@ class Laser:
         self.x = x
         self.y = y
         self.img = img
-        self.mask = pygame.mask.from_surface(self.img) #коллизия?
+        self.mask = pygame.mask.from_surface(self.img) #коллизия
         self.direction = direction
         self.imgRev = pygame.transform.rotate(self.img, 90)
         self.imgRec = self.imgRev.get_rect(topleft=(self.x, self.y))
@@ -368,23 +225,10 @@ class Laser:
     def draw(self, screen):
          if self.direction == "up" or self.direction == "down":
             screen.blit(self.img, (self.x, self.y))
-         elif self.direction == "left" or self.direction == "right":
-        #     # # screen.blit(pygame.transform.flip(self.img, True, True), (self.x, self.y))
-        #     # player_img = pygame.transform.rotate(self.img, 90)
-        #     # player_rec = player_img.get_rect(topleft=(self.x, self.y))
+         elif self.direction == "left" or self.direction == "right":  #если влево или право поворачив использ повернутый лазер
              screen.blit(self.imgRev, (self.x, self.y))
-        #     # pygame.display.update()
 
 
-        # player_img = pygame.transform.rotate(self.img_ship, angle)
-        # player_rec = player_img.get_rect(topleft=(self.x, self.y))  #или без аргумента topleft, тести
-        # print(player_rec, "player rect")
-        # screen.blit(player_img, player_rec.topleft)
-        # pygame.display.update()
-
-
-        # лазер должен двигаться по прямой траектории,не следовать за игроком.во время выстрела
-        #метод находит координату игрока и движется к ней,делает только один раз,во время выстрела
     def move(self, vel):     #
         if self.direction == "up":
             self.y += vel
@@ -396,24 +240,16 @@ class Laser:
             self.x -= vel
 
 
-
-
-
     def notOn_screen(self, height):  #удалить лазер если он вне экрана
         return  not(self.y <= height and self.y >= 0)
 
-        pass
+
 
     # создать универс. функцию collide что будет смотреть-попал ли лазер по кораблю(любому) и прикоснулся ли red ship
     # с другим кораблем.по сути смотрит на коллизию любых двух обьектов.
     # а здесь мы смотрим как поведет лазер,если он соприкоснется с кораблем(collide == True)
     def collision(self, obj):
         return Collide(obj, self)
-        pass
-
-
-
-
 
 
 #____________________________________________
@@ -421,7 +257,6 @@ class Laser:
 
 
 #проверяет столкнулись ли два обьекта(корабли лазеры с кораблями и тд)
-#мож ввести взрыв,когда два лезара врезаются в друг дргуа
 # возвращает true если произошло столкновение
 def Collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
@@ -431,14 +266,6 @@ def Collide(obj1, obj2):
 #___________________________________________
 
 
-# def Vector(obj1, obj2):   #obj1 = обьект к которому направляемся(player), obj2 = enemy
-#     print(obj1.x, obj1.y )
-#     vectorX = obj1.x - obj2.x
-#     vectorY = obj1.y - obj2.y
-#     vectorLength = math.sqrt((vectorX ** 2) + (vectorY ** 2))
-#     return vectorX, vectorY
-
-
 class Vector:
     def __init__(self, obj1, obj2):   #obj1 куда, obj2 откуда
         self.pointX = obj1.x - obj2.x
@@ -446,7 +273,6 @@ class Vector:
 
     def Length(self):
         return math.sqrt((self.pointX**2) + (self.pointY**2))
-
 
 
 
@@ -474,17 +300,16 @@ def mainMenu():
                 main()
     pygame.quit()
 
-#мож сделать цикл в while game: где есть цикл где каждый обьект в enemies проверяется с Collide,если да то исполь метод
 
-# и цикл для лазеров(lasers) что проверяет вышли ли они за предел экрана.думаю такая себе идея,столько лазеров возможны лаги
-#может ввести метод(notOn_screen) в функцию attack
+# и цикл для лазеров(lasers) что проверяет вышли ли они за предел экрана.
+#может ввести метод(notOn_screen) в функцию attack?
 
 def main():
     game = True
     lives = 5
     level = 0
     font = pygame.font.SysFont("comicsans", 50)
-    font_game_over = pygame.font.SysFont("comicsans", 20)  #?поменять цвет?
+    font_game_over = pygame.font.SysFont("comicsans", 20)
     fps = 60
     clock = pygame.time.Clock()
 
@@ -495,15 +320,12 @@ def main():
     laser_vel = 8
     player = Player(300, 650)
 
-
     game_over = False    #перемення что бы знать когда создавать меню при проигрыше,перезагружать игру
-
-
 
     #________________________________________
     def window_redraw():   #выписать HP игрока на экран!!!
         screen.blit(background, (0, 0)) #на surface screen перемещаем изобр background на координаты 0, 0
-        label_enem = font.render(f"Enemy remaining: {len(enemies)}", 1, (255, 255, 255)) #создаем surface с текстом
+        label_enem = font.render(f"Enemies remaining: {len(enemies)}", 1, (255, 255, 255)) #создаем surface с текстом
         label_level = font.render(f"Level: {level}", 1, (255, 255, 255))
         screen.blit(label_level, (10, 10))
         screen.blit(label_enem, (width - label_enem.get_width(), 10)) # накладываем surface на экран
@@ -512,9 +334,8 @@ def main():
             enemy.draw(screen)
 
         player.draw(screen)
-        time_start = pygame.time.get_ticks()
 
-        if game_over:    #ввсти выход в меню и удалить всех врагов+вернуть hp игроку
+        if game_over:    #ввсти выход в меню
             label_game_over = font.render("YOU DIED", 1, (255, 0, 0))
             label_game_over_comm = font_game_over.render("прям как в DarkSouls,понял да", 1, (255, 255, 255))
             label_game_over_space = font_game_over.render("press space to start over", 1, (255, 255, 255))
@@ -523,62 +344,35 @@ def main():
             screen.blit(label_game_over_comm, (width - label_game_over_comm.get_width()-10, height-10-label_game_over_comm.get_height()))
             screen.blit(label_game_over_space, (width / 2 - label_game_over_space.get_width()/2 , height-20))
 
+            player.stop()
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_SPACE]:
                 mainMenu()  # ДАТЬ ТАЙМЕР
 
-
-
-
-
         pygame.display.update()
     #_____________________________________
-
-    # мож сделать цикл в while game: где есть цикл где каждый обьект в enemies проверяется с Collide,если да то исполь метод
 
     while game:
         clock.tick(fps)
         window_redraw()
 
-        if lives <= 0 or player.hp <= 0:  #!!game over screen!!  game_over = true. ввсти выход в меню и удалить всех врагов+вернуть hp игроку
+        if lives <= 0 or player.hp <= 0:
             game_over = True
 
-
-        # также здесь можно организовать переходна новый уровень
-        #спавн врагов  !!!на будущие- сделай спавн врагов рандомным,что бы на каждой новой волне было разное кол врагов
-        # random.choice(["red","blue", "green"])
-
-
-        if len(enemies) == 0:   #также если игрок все еще не начал новую игру(game_over=true)не спавнить врагов
+        if len(enemies) == 0:
             level += 1
             for i in range(wave_length):
-                enemy = RedEnemy(random.randrange(50, width-100), random.randrange(10, 150), "red", player) #!!!!!!!!!!!!!!!redenemy = enemy
+                enemy = RedEnemy(random.randrange(50, width-100), random.randrange(10, 150), "red", player)
                 enemies.append(enemy)
-                # print(enemy.color_map)
 
-            for i in range(wave_length):
-                enemy = BlueEnemy(random.randrange(50, width - 100), random.randrange(10, 150),
-                              random.choice(["blue"]))  #!!!
+                enemy = BlueEnemy(random.randrange(50, width - 100), random.randrange(10, 150),random.choice(["blue"]))  #!!!
                 enemies.append(enemy)
-                # print(enemy.color_map)
-
-        #game = False
 
 
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 quit()
-
-            if event.type == pygame.MOUSEMOTION:
-                pass
-              #player.rotateShip(player.x, player.y, event.pos)
-              #print(event.pos, "MOUSE POSITIOM")
-
-
-
-
 
 #управление
         keys = pygame.key.get_pressed()
@@ -590,11 +384,6 @@ def main():
             player.y += player.vel
         if keys[pygame.K_w] and player.y - player.vel >0:
             player.y -= player.vel
-        if keys[pygame.K_SPACE]: #лазер формируется только один раз,мож проблема в cooldown?
-            pass
-            # print('shoooot')    #в будущем ввести ускорение которое тратит hp
-            # player.shoot("up")
-            #player.rotateShip(50)
         if keys[pygame.K_UP]:
             player.shoot("up")
         if keys[pygame.K_DOWN]:
@@ -605,32 +394,10 @@ def main():
             player.shoot("right")
 
 
-
-
-
-# здесь нужно ввест координату игрока в метод attack(сделал метод give_position у ship- можно применить у всех кораблей)
-        # в attack ввести метод laser_draw
-
         for enemy in enemies:
-            #enemy.move(player)
             enemy.attack(player, laser_vel, player)
 
 
-
-
-
-            #enemy.attack(player.x, player.y, laser_vel, player)  # всунули laser_draw в attack
-
-
-            #старая версия,всунул все в метод attack
-            #enemy.laser_draw(laser_vel, player)
-            # if random.randrange(0, 240) == 1:
-            #     enemy.shoot()  # игрок получает урон но он не отображается на экране+некоторые корабли вообще не стреляют
-
-
-            #здесь настроить поведение для камикадзе
-            #если collide = true и враг = redenemy.запустить метод explode для red enemy.написать метод explode
-            # ну или просто в петле вписать,но мне кажется сделать отдельный метод explode будет красивее
         for enemy in enemies:
             if enemy.colorShip == "red":
                 if Collide(enemy, player):
@@ -644,42 +411,16 @@ def main():
                     enemies.remove(enemy)
 
 
-
-
-
-
-
         player.laser_draw(-laser_vel, enemies)
-
-
-
-        #print(player.give_position())
-        #print(enemies[0].give_position())
-
 
 
 
 mainMenu()
 
-# как получить расположение мыши
-# pg.mouse.get_pos()
-#
-#сделать коллизию - взрыв камикадзе СТРОКА 69
-#сделать стрельу для корабля игрока и убийство врагов
-#
-#!!часто игра начинает тормозить!! похоже это происходить при коллизии с врагами.через час проблема пропала,мож беда в ноуте самом?
-#
-#class Enemy передеать зачем нам теперь colormap?
-#можно ли коллизию(self.mask = pygame.mask.from_surface(self.img)) поместить не в отдельные классы кораблей
-#а в общий класс Ship? !!может поместить mask в класс Enemy,там уже определяют изображение кораблям
 
-#идея как все организоваать у двух классов врагов разные методы attack но им обоим нужны координаты игрока
-#
-#переделать метод move в классе laser.надо в метод move или attack всунуть координаты игрока,что бы расчитать вектор
-#метод attack в классах redEnemy, blueEnemy
-#shoot в class SHip
-#
-#
+
+
+
 
 
 
